19d74ea787e227704c253138a26d6a75bace211c
Jack Yang <jackyangzzh@gmail.com>
2021-05-30 15:10:21 -0500

    Added SimpleMovement.cs script

---
+++ Monocular Demo/Assets/Scripts/SimpleMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // Change Sin function to the desire axis
+        transform.position = new Vector3(Mathf.Sin(Time.time), 0, 0);
+    }
+}
